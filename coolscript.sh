7f25ebe23c74318976f4339be6d46313861b1f95
ron@sandcrater.com <>
2025-02-28 12:38:51 -0500

    Start help system

--- coolscript.sh
+++ coolscript.sh
@@ -2,3 +2,7 @@
 # coolscript.sh
 
 echo "Welcome to coolscript.sh, you are cool!"
+if [[ "$1" == "help" ]]; then
+    echo "coolscript: help"
+    echo "This command will show you how cool you are, just run it!"
+fi
